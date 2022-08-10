30a66f3e94cc468ad2758780a96b6c18c02d7148
Mark <maoke@enjoyingedu.onaliyun.com>
2022-08-10 14:43:18 +0800

    数据同步初测版

---
+++ tulingmall-canal/src/main/java/com/tuling/tulingmall/config/PromotionRedisKey.java
@@ -0,0 +1,64 @@
+package com.tuling.tulingmall.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+
+@Slf4j
+@Service
+public class PromotionRedisKey {
+
+    @Value ("${namespace.promotion:prmtd}")
+    private String promotionNamespace;
+
+    @Value ("${promotion.brand:br}")
+    private String brand;
+
+    @Value ("${promotion.newProduct:np}")
+    private String newProduct;
+
+    @Value ("${promotion.recProduct:rp}")
+    private String recProduct;
+
+    @Value ("${promotion.homeAdvertise:hd}")
+    private String homeAdvertise;
+
+    private String brandKey;
+    private String newProductKey;
+    private String recProductKey;
+    private String homeAdvertiseKey;
+
+    @PostConstruct
+    public void initKey(){
+        brandKey = promotionNamespace + "." + brand;
+        newProductKey = promotionNamespace + "." + newProduct;
+        recProductKey = promotionNamespace + "." + recProduct;
+        homeAdvertiseKey = promotionNamespace + "." + homeAdvertise;
+        StringBuilder logKeyStr = new StringBuilder();
+        logKeyStr.append("[品牌推荐redis主键=").append(brandKey)
+                .append("] [新品推荐redis主键=").append(newProductKey)
+                .append("] [人气推荐redis主键=").append(recProductKey)
+                .append("] [轮播广告redis主键=").append(homeAdvertiseKey)
+                .append("]");
+        log.info("促销系统Redis主键配置：{}",logKeyStr);
+    }
+
+    public String getBrandKey() {
+        return brandKey;
+    }
+
+    public String getNewProductKey() {
+        return newProductKey;
+    }
+
+    public String getRecProductKey() {
+        return recProductKey;
+    }
+
+    public String getHomeAdvertiseKey() {
+        return homeAdvertiseKey;
+    }
+
+}
--- tulingmall-canal/src/main/java/com/tuling/tulingmall/util/ClassUtil.java
+++ tulingmall-canal/src/main/java/com/tuling/tulingmall/util/ClassUtil.java
@@ -13,10 +13,6 @@ import java.text.SimpleDateFormat;
 import java.util.*;
 
 /**
- * @author ：图灵学院
- * @date ：Created in 2020/3/9
- * @version: V1.0
- * @slogan: 天下风云出我辈，一入代码岁月催
  * @description: 反射工具类,根据column调用相关方法
  **/
 @Slf4j
