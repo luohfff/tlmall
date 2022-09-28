c741ab01bebf897043dcf81786717c1d0854799b
fox <2763800211@qq.com>
2022-09-28 15:41:44 +0800

    tulingmall-member接入spring session解决验证码共享存储的问题

--- tulingmall-member/src/main/java/com/tuling/TulingmallMemberApplication.java
+++ tulingmall-member/src/main/java/com/tuling/TulingmallMemberApplication.java
@@ -3,8 +3,10 @@ package com.tuling;
 import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication(exclude = DruidDataSourceAutoConfigure.class)
+@EnableRedisHttpSession //开启springsession
 public class TulingmallMemberApplication {
 
 	public static void main(String[] args) {
